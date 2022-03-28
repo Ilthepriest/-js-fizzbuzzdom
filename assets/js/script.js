0058f57c390512f00dbbc2335c8ca378af7b1d51
Ilthepriest <robertodelprete1993@libero.it>
2022-03-28 16:47:51 +0200

    add exercise

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
+const element_table = document.querySelector(".grid");
+
+
+for(let i = 1; i <= 100 ; i++){
+    if(i % 15 === 0){
+        let cella = `<div class="cell  fizzbuzz square--${i}">fizzbuzz</div>`    
+        element_table.insertAdjacentHTML("beforeend", cella);
+    }else if (i % 3 === 0){
+        let cella = `<div class="cell  fizz square--${i}">fizz</div>`    
+        element_table.insertAdjacentHTML("beforeend", cella);
+    }else if (i % 5 === 0){
+        let cella = `<div class="cell buzz square--${i}">buzz</div>`    
+        element_table.insertAdjacentHTML("beforeend", cella);
+    }else{
+        let cella = `<div class="cell square--${i}">${i}</div>`    
+        element_table.insertAdjacentHTML("beforeend", cella);
+    }
+}
+
 
